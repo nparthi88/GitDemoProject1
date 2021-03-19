b060ad5b70176eff658048e09d0bf759874ebfaa
parthiban <parthiba88@outlook.com>
2021-03-19 10:33:13 +0530

    first commit

---
+++ src/com/parthi/Test1.java
@@ -0,0 +1,18 @@
+package com.parthi;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hungry";
+		if (s=="Hungry")
+		{
+			System.out.println("Eat something...");
+		}
+		else
+		{
+			System.out.println("do nothing");
+		}
+	}
+
+}
